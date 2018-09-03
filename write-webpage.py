c5ea174b9b322bc7081ff2a0dbb8d898f5d074df
Dave Coate <coateds@outlook.com>
2018-09-03 15:21:18 -0700

    still working with writing the whole webpage

--- write-webpage.py
+++ write-webpage.py
@@ -18,7 +18,7 @@ count = 0
 
 while True:
     count += 1
-    index = open('/etc/nginx/html/index.html', 'a')
+    index = open('/etc/nginx/html/index.html', 'W')
     # dynamicstring = htmlheader + htmlbodytop + "<p>dynamic line" + count + "</p>" + htmlbodybottom
     # index.write(dynamicstring)
     index.write(htmlwhole)
