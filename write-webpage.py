a7af222ce3ce86df16ab4b0c8d6f9b2d521608e0
Dave Coate <coateds@outlook.com>
2018-09-03 14:55:23 -0700

    first cut at write-webpage

--- write-webpage.py
+++ write-webpage.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python
 import time
+
+htmlheader = "<html><head></head>"
+htmlbodytop = "<body>"
+htmlbodybottom = "</body></html>"
+
+count = 0
+
+while True:
+    count += 1
+    index = open('/etc/nginx/html/index.html', 'w')
+    dynamicstring = htmlheader + htmlbodytop + "<p>dynamic line" + count + "</p>" + htmlbodybottom
+    index.write(dynamicstring)
+    index.close
+    time.sleep(2)    
